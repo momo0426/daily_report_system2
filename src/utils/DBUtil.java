2431b4d8e6cfb3f0c7ab6e2af0f2e68601df308f
momoko.kuwabara <mm.dgdo@gmail.com>
2021-06-23 23:52:04 +0900

    Add Employee index

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENSE_UNIT_NAME = "daily_report_system";
+    private static final String PERSISTENCE_UNIT_NAME = "daily_report_system";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
@@ -14,7 +14,7 @@ public class DBUtil {
 
     public static EntityManagerFactory getEntityManagerFactory() {
         if(emf == null){
-            emf = Persistence.createEntityManagerFactory(PERSISTENSE_UNIT_NAME);
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
 
         return emf;
