eed10da6ecc06b73a36d7504741957e91c7317cb
momo0426 <mm.dgdo@gmail.com>
2021-07-28 21:47:08 +0900

    Add Department new

--- src/controllers/department/DepartmentIndexServlet.java
+++ src/controllers/department/DepartmentIndexServlet.java
@@ -1,41 +1,62 @@
 package controllers.department;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Department;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class DepartmentIndexServlet
  */
 @WebServlet("/departments/index")
 public class DepartmentIndexServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public DepartmentIndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        int page = 1;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException dp) { }
+        List<Department> departments = em.createNamedQuery("getAllDepartments", Department.class)
+                                     .setFirstResult(15 * (page - 1))
+                                     .setMaxResults(15)
+                                     .getResultList();
 
+        long departments_count = (long)em.createNamedQuery("getDepartmentsCount", Long.class)
+                                       .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("departments", departments);
+        request.setAttribute("departments_count", departments_count);
+        request.setAttribute("page", page);
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/departments/index.jsp");
+        rd.forward(request, response);
+    }
 }
